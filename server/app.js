e5daa8722f703560b4d99397935e6ecb09117d9e
Cherylyli <cherylliat@gmail.com>
2016-06-11 12:18:58 -0400

    simple server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+//var config = require('./config/index');
+var mongoose = require('mongoose');
+
+app.get('/', function(req, res){
+   res.send("testing");
+});
+
+process.env.PORT = process.env.PORT || 80;
+
+app.listen(process.env.PORT, function(){
+    console.log("running on " + process.env.PORT);
+});
