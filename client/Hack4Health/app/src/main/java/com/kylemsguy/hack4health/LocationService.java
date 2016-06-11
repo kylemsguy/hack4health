0438bbf04d5c5962102cb5f380edf3975943ffaf
Kyle <kylezhou2002@hotmail.com>
2016-06-11 17:34:51 -0400

    Fixing some compile issues

--- client/Hack4Health/app/src/main/java/com/kylemsguy/hack4health/LocationService.java
+++ client/Hack4Health/app/src/main/java/com/kylemsguy/hack4health/LocationService.java
@@ -4,42 +4,34 @@ import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.PowerManager;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-public class LocationService extends IntentService {
-    public static final String EMAIL_
-    private ApiWrapper apiWrapper;
-    /**
-     * A constructor is required, and must call the super IntentService(String)
-     * constructor with a name for the worker thread.
-     */
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class LocationService extends Service {
+
     public LocationService() {
-        super("LocationService");
-        apiWrapper = ApiWrapper.getInstance();
+
     }
 
-    /**
-     * The IntentService calls this method from the default worker thread with
-     * the intent that started the service. When this method returns, IntentService
-     * stops the service, as appropriate.
-     */
     @Override
-    protected void onHandleIntent(Intent intent) {
-        // Normally we would do some work here, like download a file.
-        // For our sample, we just sleep for 5 seconds.
-        Bundle bundle = intent.getStringExtra()
-        try {
-            apiWrapper.sendLocationToServer()
-        } catch (InterruptedException e) {
-            // Restore interrupt status.
-            Thread.currentThread().interrupt();
-        }
+    public void onCreate() {
+        super.onCreate();
+        
     }
 
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
