6b9b7b2e70ea8458641033669fb7415998dd2f2e
Kyle <kylezhou2002@hotmail.com>
2016-06-11 15:09:52 -0400

    Implementing login for app

--- client/Hack4Health/app/src/main/java/com/kylemsguy/hack4health/ApiWrapper.java
+++ client/Hack4Health/app/src/main/java/com/kylemsguy/hack4health/ApiWrapper.java
@@ -1,38 +1,73 @@
 package com.kylemsguy.hack4health;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by kyle on 11/06/16.
  */
 public class ApiWrapper {
     public static final String BASE = "";
+    public static final String LOGIN = BASE + "login/";
+    public static final MediaType MEDIA_TYPE_JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    private static ApiWrapper instance;
+
+    private OkHttpClient client;
 
     private ApiWrapper(){
-
+        client = new OkHttpClient();
     }
 
-    public static String login(String email, String password){
-        return null;
-    }
-
-    /**
-     * Sends a GET request to URL
-     * @param url
-     * @param params
-     * @return
-     */
-    public static String getPage(String url, Map<String, String> params){
-        return null;
+    public static ApiWrapper getInstance(){
+        if(instance == null) {
+            instance = new ApiWrapper();
+        }
+        return instance;
     }
 
     /**
-     * Sends a POST request to URL and returns the result
-     * @param url
-     * @param params
+     * Requests a list of appointments associated with the provided email.
+     *
+     * @param email
+     * @param password
      * @return
+     * @throws IOException
      */
-    public static String sendPost(String url, Map<String, String> params){
-        return null;
+    public List<LoginResponse> login(String email, String password) throws IOException {
+        JsonObject json = new JsonObject();
+        json.addProperty("email", email);
+        json.addProperty("password", password);
+
+        Request request = new Request.Builder()
+                .url(LOGIN)
+                .post(RequestBody.create(MEDIA_TYPE_JSON, json.toString()))
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if(!response.isSuccessful()){
+            throw new IOException("Unexpected code " + response);
+        }
+
+        String responseStr = response.body().string();
+
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<List<LoginResponse>>(){}.getType();
+        return gson.fromJson(json, collectionType);
     }
 }
---
+++ client/Hack4Health/app/src/main/java/com/kylemsguy/hack4health/LoginResponse.java
@@ -0,0 +1,22 @@
+package com.kylemsguy.hack4health;
+
+/**
+ * Created by kyle on 11/06/16.
+ */
+public class LoginResponse {
+    private int appid;
+    private String clinicName;
+    private String time;
+
+    public int getAppid() {
+        return appid;
+    }
+
+    public String getClinicName() {
+        return clinicName;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
