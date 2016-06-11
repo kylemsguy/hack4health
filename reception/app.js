a6bee4d97da1002ad0a09fa828a3d2967cb92e5a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-06-11 16:25:46 -0400

    Load data for appointment page

--- reception/app.js
+++ reception/app.js
@@ -1,18 +1,47 @@
-angular.module("app", ["ngRoute", ])
-.controller("LoginController", function($location) {
+var BACKEND_URL="https://hack4health-cherylyli.c9users.io";
+angular.module("app", ["ngRoute", "ngResource", "ngCookies"])
+.controller("LoginController", function($rootScope, $location, $cookies) {
 	var login = this;
 	login.submit = function() {
+		$cookies.put("clinicName", login.loginData.clinicName);
 		$location.path("/appointments");
 	}
+	login.clinicNames = ["Markham Family Health Team", "Test 2"];
+	login.loginData = {clinicName: $cookies.get("clinicName")? $cookies.get("clinicName"): login.clinicNames[0]};
 })
-.controller("AppointmentsController", function() {
-	var appointments = this;
-	appointments.activeAppointments = [{
-		id: "1234",
-		time: "9:30",
-		name: "Example",
-		distance: "1.234 km"
-	}];
+.controller("AppointmentsController", function($rootScope, $location, $http, $cookies) {
+	var self = this;
+	self.activeAppointments = [];
+	self.clinicName = $cookies.get("clinicName");
+	self.date = new Date();
+	self.refresh = function() {
+		if (!self.clinicName) {
+			$location.path("/");
+			return;
+		}
+		$http.post(BACKEND_URL + "/clinicLogin", {
+			clinicName: self.clinicName,
+			month: self.date.getMonth() + 1,
+			day: 12//self.date.getDate()
+		})
+		.then(function(response) {
+			console.log(response);
+			self.activeAppointments = response.data;
+		}, function(error) {
+		})
+	}
+	self.formatTime = function(time) {
+		var firstPart = Math.floor(time);
+		var fractionalPart = time - firstPart;
+		var minutes = (fractionalPart * 60).toString();
+		if (minutes.length < 2) {
+			minutes = "0" + minutes;
+		}
+		return firstPart + ":" + minutes;
+	}
+	self.refresh();
+})
+.controller("CreateAppointmentController", function() {
 })
 .config(function($routeProvider) {
 	$routeProvider
@@ -23,6 +52,10 @@ angular.module("app", ["ngRoute", ])
 		.when("/appointments", {
 			templateUrl: "appointments.html",
 			controller: "AppointmentsController as appointments"
+		})
+		.when("/createappointment", {
+			templateUrl: "createappointment.html",
+			controller: "CreateAppointmentController as createAppointments"
 		})
 		.otherwise({
 			redirectTo: "/"
