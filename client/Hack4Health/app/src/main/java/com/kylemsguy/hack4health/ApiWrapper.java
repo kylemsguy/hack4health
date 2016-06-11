7bcbdcd29aeee04613b03368ac409aa988dca6d5
Kyle <kylezhou2002@hotmail.com>
2016-06-11 14:18:26 -0400

    Initial commit of Android project

---
+++ client/Hack4Health/app/src/main/java/com/kylemsguy/hack4health/ApiWrapper.java
@@ -0,0 +1,38 @@
+package com.kylemsguy.hack4health;
+
+import java.util.Map;
+
+/**
+ * Created by kyle on 11/06/16.
+ */
+public class ApiWrapper {
+    public static final String BASE = "";
+
+    private ApiWrapper(){
+
+    }
+
+    public static String login(String email, String password){
+        return null;
+    }
+
+    /**
+     * Sends a GET request to URL
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String getPage(String url, Map<String, String> params){
+        return null;
+    }
+
+    /**
+     * Sends a POST request to URL and returns the result
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String sendPost(String url, Map<String, String> params){
+        return null;
+    }
+}
